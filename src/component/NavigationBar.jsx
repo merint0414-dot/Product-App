f2b0a252b99226d2f22a2ba29241943d3023781e
merint0414-dot <merint0414@gmail.com>
2026-07-11 17:35:59 +0530

    First Push Of Product App

--- src/component/NavigationBar.jsx
+++ src/component/NavigationBar.jsx
@@ -3,20 +3,20 @@ import React from 'react'
 const NavigationBar = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div class="navbar-nav">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            <a class="nav-link" href="/">Add Product</a>
-                            <a class="nav-link" href="/search">Search Product</a>
-                            <a class="nav-link" href="/delete">Delete Product</a>
-                            <a class="nav-link" href="/view">View Product</a>
-                            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <a className="nav-link" href="/">Add Product</a>
+                            <a className="nav-link" href="/search">Search Product</a>
+                            <a className="nav-link" href="/delete">Delete Product</a>
+                            <a className="nav-link" href="/view">View Product</a>
+                            <a className="nav-link disabled" aria-disabled="true">Disabled</a>
                         </div>
                     </div>
                 </div>
--- src/component/ViewProduct.jsx
+++ src/component/ViewProduct.jsx
@@ -12,73 +12,73 @@ const ViewProduct = () => {
                             <div className="card-body">
                                 <div className="row g-3">
                                     <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-                                        <div class="card" >
-                                            <img src="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D" class="card-img-top" alt="..." height="180px" width="200px"/>
-                                            <div class="card-body">
-                                                <h5 class="card-title">Product Name: Headphone</h5>
-                                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                                <p class="card-text"><strong>Price: $29.99</strong></p>
-                                                <a href="#" class="btn btn-primary">Add to cart</a>
+                                        <div className="card" >
+                                            <img src="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D" className="card-img-top" alt="..." height="180px" width="200px"/>
+                                            <div className="card-body">
+                                                <h5 className="card-title">Product Name: Headphone</h5>
+                                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                                                <p className="card-text"><strong>Price: $29.99</strong></p>
+                                                <a href="#" className="btn btn-primary">Add to cart</a>
                                             </div>
                                         </div>
                                     </div>
 
                                     <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-                                        <div class="card" >
-                                            <img src="https://images.unsplash.com/photo-1541643600914-78b084683601?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D" class="card-img-top" alt="..." height="180px" width="200px"/>
-                                            <div class="card-body">
-                                                <h5 class="card-title">Product Name: Headphone</h5>
-                                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                                <p class="card-text"><strong>Price: $29.99</strong></p>
-                                                <a href="#" class="btn btn-primary">Add to cart</a>
+                                        <div className="card" >
+                                            <img src="https://images.unsplash.com/photo-1541643600914-78b084683601?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D" className="card-img-top" alt="..." height="180px" width="200px"/>
+                                            <div className="card-body">
+                                                <h5 className="card-title">Product Name: Headphone</h5>
+                                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                                                <p className="card-text"><strong>Price: $29.99</strong></p>
+                                                <a href="#" className="btn btn-primary">Add to cart</a>
                                             </div>
                                         </div>
                                     </div>
 
                                     <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-                                        <div class="card" >
-                                            <img src="https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="..." height="180px" width="200px"/>
-                                            <div class="card-body">
-                                                <h5 class="card-title">Product Name: Headphone</h5>
-                                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                                <p class="card-text"><strong>Price: $29.99</strong></p>
-                                                <a href="#" class="btn btn-primary">Add to cart</a>
+                                        <div className="card" >
+                                            <img src="https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="..." height="180px" width="200px"/>
+                                            <div className="card-body">
+                                                <h5 className="card-title">Product Name: Headphone</h5>
+                                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                                                <p className="card-text"><strong>Price: $29.99</strong></p>
+                                                <a href="#" className="btn btn-primary">Add to cart</a>
                                             </div>
                                         </div>
                                     </div>
 
                                     <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-                                        <div class="card" >
-                                            <img src="https://images.unsplash.com/photo-1560343090-f0409e92791a?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="..." height="180px" width="200px"/>
-                                            <div class="card-body">
-                                                <h5 class="card-title">Product Name: Headphone</h5>
-                                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                                <p class="card-text"><strong>Price: $29.99</strong></p>
-                                                <a href="#" class="btn btn-primary">Add to cart</a>
+                                        <div className="card" >
+                                            <img src="https://images.unsplash.com/photo-1560343090-f0409e92791a?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="..." height="180px" width="200px"/>
+                                            <div className="card-body">
+                                                <h5 className="card-title">Product Name: Headphone</h5>
+                                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                                                <p className="card-text"><strong>Price: $29.99</strong></p>
+                                                <a href="#" className="btn btn-primary">Add to cart</a>
                                             </div>
                                         </div>
                                     </div>
 
                                     <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-                                        <div class="card" >
-                                            <img src="https://images.unsplash.com/photo-1580870069867-74c57ee1bb07?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="..." height="180px" width="200px"/>
-                                            <div class="card-body">
-                                                <h5 class="card-title">Product Name: Headphone</h5>
-                                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                                <p class="card-text"><strong>Price: $29.99</strong></p>
-                                                <a href="#" class="btn btn-primary">Add to cart</a>
+                                        <div className="card" >
+                                            <img src="https://images.unsplash.com/photo-1580870069867-74c57ee1bb07?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="..." height="180px" width="200px"/>
+                                            <div className="card-body">
+                                                <h5 className="card-title">Product Name: Headphone</h5>
+                                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                                                <p className="card-text"><strong>Price: $29.99</strong></p>
+                                                <a href="#" className="btn btn-primary">Add to cart</a>
                                             </div>
                                         </div>
                                     </div>
 
                                     <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-                                        <div class="card" >
-                                            <img src="https://images.unsplash.com/photo-1491637639811-60e2756cc1c7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D" class="card-img-top" alt="..." height="200px" width="200px"/>
-                                            <div class="card-body">
-                                                <h5 class="card-title">Product Name: Headphone</h5>
-                                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                                <p class="card-text"><strong>Price: $29.99</strong></p>
-                                                <a href="#" class="btn btn-primary">Add to cart</a>
+                                        <div className="card" >
+                                            <img src="https://images.unsplash.com/photo-1491637639811-60e2756cc1c7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D" className="card-img-top" alt="..." height="200px" width="200px"/>
+                                            <div className="card-body">
+                                                <h5 className="card-title">Product Name: Headphone</h5>
+                                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                                                <p className="card-text"><strong>Price: $29.99</strong></p>
+                                                <a href="#" className="btn btn-primary">Add to cart</a>
                                             </div>
                                         </div>
                                     </div>

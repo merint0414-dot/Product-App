fb21db384db3157b40accff8f695eb7d36318f5e
merint0414-dot <merint0414@gmail.com>
2026-07-12 09:20:00 +0530

    Updated

--- src/component/ViewProduct.jsx
+++ src/component/ViewProduct.jsx
@@ -1,95 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavigationBar from './NavigationBar'
 
 const ViewProduct = () => {
+    const [data, changeData] = useState([
+        {
+            name: "Wireless Headphones",
+            avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuAbxJg56uoWYQxMxXNM_CyOUQvcJ6YYok60flxyb_Sg&s=10",
+            price: 2999
+        },
+        {
+            name: "Smart Watch",
+            avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUmp0urh_oV5g1rZZI_TYNwt55bs4mJpN_OOt9SnMUgQ&s=10",
+            price: 4999
+        },
+        {
+            name: "Bluetooth Speaker",
+            avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGZ5nxA5amtgWyff1UYdKuvXowdWb8wIqaw4pXJCVRyQ&s=10",
+            price: 1899
+        },
+        {
+            name: "Gaming Mouse",
+            avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpHf_7TNUk1FR2qs4nQTNmqqC5pCtP4iLRHD-NzlQdSA&s=10",
+            price: 1299
+        },
+        {
+            name: "Laptop Backpack",
+            avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnp4oIh1bygJ6hs5D5yGZ0V3SGgR_kSzB9zRe_JB-Tjg&s=10",
+            price: 1799
+        },
+        {
+            name: "USB-C Charger",
+            avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTGzKRuiKfB-6Pz9VoXpQZHVWKneGXHZFoJqaLXS1Kzw&s=10",
+            price: 999
+        }
+    ]);
+
     return (
         <div>
-            <NavigationBar/>
-            <div className="containter">
+            <NavigationBar />
+
+            <div className="container mt-3">
                 <div className="row">
-                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl 12 col-xxl-12">
-                        <div className="card shadow">
-                            <div className="card-body">
-                                <div className="row g-3">
-                                    <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-                                        <div className="card" >
-                                            <img src="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D" className="card-img-top" alt="..." height="180px" width="200px"/>
-                                            <div className="card-body">
-                                                <h5 className="card-title">Product Name: Headphone</h5>
-                                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                                <p className="card-text"><strong>Price: $29.99</strong></p>
-                                                <a href="#" className="btn btn-primary">Add to cart</a>
-                                            </div>
-                                        </div>
-                                    </div>
+                    <div className="col-12">
+                        <div className="row g-3">
 
-                                    <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-                                        <div className="card" >
-                                            <img src="https://images.unsplash.com/photo-1541643600914-78b084683601?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D" className="card-img-top" alt="..." height="180px" width="200px"/>
-                                            <div className="card-body">
-                                                <h5 className="card-title">Product Name: Headphone</h5>
-                                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                                <p className="card-text"><strong>Price: $29.99</strong></p>
-                                                <a href="#" className="btn btn-primary">Add to cart</a>
-                                            </div>
-                                        </div>
-                                    </div>
+                            {data.map((value, index) => (
+                                <div
+                                    className="col-12 col-sm-6 col-md-6 col-lg-3"
+                                    key={index}
+                                >
+                                    <div className="card h-100 shadow-sm">
+                                        <img
+                                            src={value.avatar}
+                                            className="card-img-top"
+                                            alt={value.name}
+                                            height="200"
+                                        />
 
-                                    <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-                                        <div className="card" >
-                                            <img src="https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="..." height="180px" width="200px"/>
-                                            <div className="card-body">
-                                                <h5 className="card-title">Product Name: Headphone</h5>
-                                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                                <p className="card-text"><strong>Price: $29.99</strong></p>
-                                                <a href="#" className="btn btn-primary">Add to cart</a>
-                                            </div>
-                                        </div>
-                                    </div>
+                                        <div className="card-body d-flex flex-column">
+                                            <h5 className="card-title">
+                                                {value.name}
+                                            </h5>
 
-                                    <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-                                        <div className="card" >
-                                            <img src="https://images.unsplash.com/photo-1560343090-f0409e92791a?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="..." height="180px" width="200px"/>
-                                            <div className="card-body">
-                                                <h5 className="card-title">Product Name: Headphone</h5>
-                                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                                <p className="card-text"><strong>Price: $29.99</strong></p>
-                                                <a href="#" className="btn btn-primary">Add to cart</a>
-                                            </div>
-                                        </div>
-                                    </div>
+                                            <p className="card-text">
+                                                <strong>Price:</strong> ₹{value.price}
+                                            </p>
 
-                                    <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-                                        <div className="card" >
-                                            <img src="https://images.unsplash.com/photo-1580870069867-74c57ee1bb07?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="..." height="180px" width="200px"/>
-                                            <div className="card-body">
-                                                <h5 className="card-title">Product Name: Headphone</h5>
-                                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                                <p className="card-text"><strong>Price: $29.99</strong></p>
-                                                <a href="#" className="btn btn-primary">Add to cart</a>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-                                        <div className="card" >
-                                            <img src="https://images.unsplash.com/photo-1491637639811-60e2756cc1c7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D" className="card-img-top" alt="..." height="200px" width="200px"/>
-                                            <div className="card-body">
-                                                <h5 className="card-title">Product Name: Headphone</h5>
-                                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                                <p className="card-text"><strong>Price: $29.99</strong></p>
-                                                <a href="#" className="btn btn-primary">Add to cart</a>
-                                            </div>
+                                            <button className="btn btn-success mt-auto w-100">
+                                                Add to Cart
+                                            </button>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
+                            ))}
+
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default ViewProduct
